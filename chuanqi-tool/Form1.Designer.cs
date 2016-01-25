94e588b4432f4339b077ccd13c6c377514beaf0a
zuifengke <yehui1989@qq.com>
2016-01-25 13:02:44 +0800

    定时关闭进程

--- chuanqi-tool/Form1.Designer.cs
+++ chuanqi-tool/Form1.Designer.cs
@@ -29,11 +29,114 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(600, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(33, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(158, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(436, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "可执行程序的路径：";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "设置关闭时间：";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(131, 135);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(38, 163);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(62, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "启动";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(257, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "注意：程序要以管理员身份运行，才能关闭进程";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 272);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- chuanqi-tool/Form1.cs
+++ chuanqi-tool/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
@@ -13,6 +14,67 @@ namespace chuanqi_tool
         public Form1()
         {
             InitializeComponent();
+        }
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+            this.dateTimePicker1.Value = DateTime.Now;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox1.Text = dialog.FileName;
+            }
+                
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Closeprocess();
+        }
+        public void Closeprocess()
+        {
+            if (this.textBox1.Text == string.Empty)
+                return;
+            foreach (Process p in Process.GetProcesses())
+            {
+                if (p.MainWindowHandle == IntPtr.Zero) continue;
+                Console.WriteLine(p.MainModule.FileName);
+                if (p.MainModule.FileName == this.textBox1.Text)
+                {
+                    p.Kill();
+                    MessageBox.Show("关闭成功");
+                    this.timer1.Stop();
+                    return;
+                }
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(this.dateTimePicker1.Value.ToString("yyyy-MM-dd HH:mm:ss")==DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"))
+            {
+                Closeprocess();
+               
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(this.button3.Text=="启动")
+            {
+                this.timer1.Start();
+                this.dateTimePicker1.Enabled = false;
+                this.button3.Text = "停止";
+            }
+            else
+
+            {
+                this.dateTimePicker1.Enabled = true;
+                this.timer1.Stop();
+                this.button3.Text = "启动";
+            }
         }
     }
 }
